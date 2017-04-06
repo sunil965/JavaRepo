4c4e6ea7620df07d0d5c507a7fcbfb78a4e75e2c
sunil965clear <sunilkumar192114@gmail.com>
2017-04-06 10:58:27 +0530

    eclipse

--- StringBserach.java
+++ StringBserach.java
@@ -1,20 +1,50 @@
 import java.io.*;
 import java.util.*;
 
-public class StringBserach{
- public static void main(String[] args) throws IOException {
-    //File file = new File("/home/bridgeit/Desktop/sample.txt");
-    //FileInputStream fis = new FileInputStream(file);
-    //String fileInput = fis;
-    //System.out.println(fileInput);
+public class StringBserach
+{
+ public static void main(String[] args) throws IOException 
+ {  
+   Scanner sc = new Scanner(System.in); 	
    String content = new Scanner(new File("/home/bridgeit/Desktop/sample.txt")).useDelimiter("\\Z").next();
    System.out.println(content);
+   System.out.println("............................");
+
    String[] strArray = content.split(" ");
    for(String s:strArray)
    {System.out.println(s); }
-System.out.println("............................");
+   System.out.println("............................");
+
    Arrays.sort(strArray);
-   for(String s:strArray)
-   {System.out.println(s); }
+   for(int i=0; i<strArray.length; i++)
+   {System.out.println(); }
+
+   System.out.println("Which word you have to search.");
+   String word = sc.next();
+
+  	int result = binarySearch(strArray, word);
+	System.out.println("Your searched word is at index "+result);
  }
+
+  public static int binarySearch(String array[], String value)
+  {
+      int first = 0;
+      int last = array.length - 1;
+  	 if(first > last) System.out.println("No words !");
+	    
+		while(first < last)
+		{
+			int mid = first + (last - first)/2;
+			if(value.compareTo(array[mid])==0)
+				return mid;
+			
+		        if(value.compareTo(array[mid])<0)
+				last = mid-1; 
+		        else
+		          first = mid+1;
+		 }
+	     return -1;
+		
+   }
 }
+
---
+++ Test/src/InsertionSortInteger.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+
+public class InsertionSortInteger
+{
+	public static void main(String[] args) {
+		InsertionSortInteger bs = new InsertionSortInteger();
+        Scanner sc = new Scanner(System.in);		
+		System.out.println("No of integers ?");
+		int number = sc.nextInt();
+		
+		int[] arr = new int[number];
+		System.out.println("Enter integers ");
+		for(int k=0; k<arr.length; k++)
+		{
+			arr[k]=sc.nextInt();
+		}
+		int n = arr.length;
+		
+		System.out.println("Array before  sorting...");
+		for(int a:arr){
+			System.out.print(a+" ");
+		}		
+		
+		bs.sort(arr, n);
+		
+		System.out.println("\n Array after sorting...");
+		for(int a:arr){
+			System.out.print(a+ " ");
+		}
+	}
+	
+	public  void sort(int a[] , int n){
+		for(int i=1; i<n-1; i++){
+			int data = a[i];
+			int hole = i;
+			while(hole>0 && a[hole-1]>data){
+				a[hole] = a[hole-1];
+				hole = hole-1;
+			}
+			a[hole] = data;
+		}
+	}
+
+}
